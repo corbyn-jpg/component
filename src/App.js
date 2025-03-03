bea7efdaf45d2b2ffe47d691737b068156dca580
corbyn-jpg <159898463+corbyn-jpg@users.noreply.github.com>
2025-03-03 14:41:38 +0200

    Setting up

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "./components/navbar";
+import Carousel from "./components/carousel";
+import Card from "./components/card";
+import Accordion from "./components/accordion";
+import Form from "./components/form";
+import "./App.css";
+
+
+function App() {
+  return (
+    
+  );
+}
+
+export default App;
