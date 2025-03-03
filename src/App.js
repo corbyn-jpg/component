97e2fb5ea3a5c5980d0b9a8b5e26d44001e88120
corbyn-jpg <159898463+corbyn-jpg@users.noreply.github.com>
2025-03-03 14:42:26 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ import Accordion from "./components/accordion";
 import Form from "./components/form";
 import "./App.css";
 
+const images = [
+  "https://placehold.co/600x400/png",
+  "https://placehold.co/600x400/orange/white",
+  "https://placehold.co/600x400/blue/black",
+];
+
 
 function App() {
   return (
